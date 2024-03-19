c394fa03abef8e08c596c6fbdcc5d6478f51fc51
Anna <sunayaka2@gmail.com>
2024-03-20 02:07:11 +0630

    Go Day 4

--- 11_structs.go
+++ 11_structs.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+/* Defining a Struct (type struct) */
 type bill struct {
 	name     string
 	items    map[string]int
@@ -16,7 +20,78 @@ func newBill(name string) bill {
 	}
 }
 
-func main() {
+/*
+Receiver Methods for Structs
+(it lets the function associates with a type)
+func (receiver) func_name output_type
+*/
+func (b *bill) format() string {
+	content := fmt.Sprintf("%-20s %s \n\n", "Bill for", "Mr/Ms "+b.name)
+	sum := 0
+
+	for name, price := range b.items {
+		content += fmt.Sprintf("%-20s %d \n", name, price)
+		sum += price
+	}
+	content += fmt.Sprintf("\n%-20s %d \n", "Sum", sum)
+
+	discount := float64(sum) * b.discount
+	total := float64(sum) - discount
+	content += fmt.Sprintf("\n%-20s %0.1f \n", "Discount", discount)
+	content += fmt.Sprintf("\n%-20s %0.1f \n", "Total", total)
+
+	return content
+}
+
+/* Reciever Methods with Pointers */
+func (b *bill) updateDiscount(discount float64) {
+	b.discount = discount
+}
+
+func (b *bill) addItem(name string, price int) {
+	b.items[name] = price
+}
+
+func (b *bill) save() {
+	data := []byte(b.format())
+	err := os.WriteFile("bills/"+b.name+".txt", data, 0644)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("file was saved")
+	}
+}
+
+func structs() {
+	/* Creating an Instance of Struct */
 	bill_1 := newBill("naruto")
+	bill_1.items = map[string]int{
+		"sushi":     1500,
+		"soba":      880,
+		"miso soup": 320,
+	}
 	fmt.Println(bill_1)
+
+	/* Accessing and Modifying Fields */
+	fmt.Println(bill_1.name)
+	bill_1.name = "sakura"
+	fmt.Println(bill_1)
+
+	/* Struct is value-type */
+	bill_2 := bill_1
+	bill_1.name = "hinata"
+	fmt.Println(bill_1, bill_2)
+	// bill_1 is hinata, bill_2 is still sakura, doesn't change
+
+	bill_3 := newBill("kiba")
+	bill_3.addItem("tonkatsu", 2200)
+	bill_3.updateDiscount(0.5)
+
+	bill_1.updateDiscount(0.1)
+	bill_1.addItem("tofu", 500)
+	bill_1.addItem("udon", 1250)
+	bill_1.addItem("natto", 250)
+
+	fmt.Println(bill_1.format())
+	fmt.Println(bill_3.format())
 }
--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getInput(reader *bufio.Reader, prompt string) (string, error) {
+	fmt.Print(prompt)
+	input, err := reader.ReadString('\n')
+	return strings.TrimSpace(input), err
+}
 
 func main() {
-	fmt.Println("hello sekai")
+	reader := bufio.NewReader(os.Stdin)
+
+	name, _ := getInput(reader, "Create a bill for: ")
+	b := newBill(name)
+
+	// Infinity looop
+	for {
+		item_name, _ := getInput(reader, "Item name: ")
+		if item_name == "" {
+			break
+		}
+		item_price, _ := getInput(reader, "Item price: ")
+		p, err := strconv.ParseInt(item_price, 10, 64)
+
+		if err != nil {
+			fmt.Println("Invalid input for item price. The Item won't be added to the bill.")
+			continue
+		} else {
+			b.addItem(item_name, int(p))
+		}
+	}
+	discount, _ := getInput(reader, "Enter discount: ")
+	if discount != "" {
+		d, err := strconv.ParseFloat(discount, 64)
+		if err != nil {
+			fmt.Println("Invalid input for discount. Discount won't be added to the bill.")
+		} else {
+			b.updateDiscount(d)
+		}
+	}
+
+	b.save()
 }
